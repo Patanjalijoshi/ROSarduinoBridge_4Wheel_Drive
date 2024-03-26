ec0c898888e3b90d624d41d55de5283593da19f7
Patanjali Joshi <86454491+Patanjalijoshi@users.noreply.github.com>
2024-03-26 12:07:52 +0530

    updating for ESP32

--- encoder_driver.h
+++ encoder_driver.h
@@ -1,17 +1,17 @@
    
 //#ifdef BLD750_ENC_Counter
 
-  #define FRONT_LEFT_ENC_PIN_A 2  
-  #define FRONT_LEFT_ENC_PIN_B 3  
+  #define FRONT_LEFT_ENC_PIN_A D33
+  #define FRONT_LEFT_ENC_PIN_B D32 
 
-  #define FRONT_RIGHT_ENC_PIN_A 18  
-  #define FRONT_RIGHT_ENC_PIN_B 19 
+  #define FRONT_RIGHT_ENC_PIN_A D22  
+  #define FRONT_RIGHT_ENC_PIN_B D23 
 
-  #define BACK_LEFT_ENC_PIN_A 20  
-  #define BACK_LEFT_ENC_PIN_B 21 
+  #define BACK_LEFT_ENC_PIN_A D27  
+  #define BACK_LEFT_ENC_PIN_B D26 
 
-  #define BACK_RIGHT_ENC_PIN_A 62 
-  #define BACK_RIGHT_ENC_PIN_B 63
+  #define BACK_RIGHT_ENC_PIN_A D18 
+  #define BACK_RIGHT_ENC_PIN_B D19
      
 //#endif
 
